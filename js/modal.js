5ba08dadd5c68cd850da3912186170522aa2d16b
James Scott McDowell <mcdowellj87@gmail.com>
2019-08-31 18:30:57 -0400

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -4,3 +4,15 @@ function showModal(){
 function hideModal(){
     document.getElementById('modal').style.display='none';
 }
+function showModal2(){
+    document.getElementById('modal2').style.display='grid';
+}
+function hideModal2(){
+    document.getElementById('modal2').style.display='none';
+}
+function showModal3(){
+    document.getElementById('modal3').style.display='grid';
+}
+function hideModal3(){
+    document.getElementById('modal3').style.display='none';
+}
